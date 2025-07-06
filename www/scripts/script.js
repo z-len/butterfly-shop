f23604f874ef1339d859f543344ea4fc1bfb9053
Elena <z-len@yandex.ru>
2025-07-06 19:35:20 +0300

    добавлено событие для меню

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,7 @@
 console.log('script work!');
+let burger = document.querySelector('.js-burger');
+burger.addEventListener('click', function(event) {
+    event.preventDefault();
+    let menu = document.querySelector('.js-menu');
+    menu.classList.add('open');
+})
