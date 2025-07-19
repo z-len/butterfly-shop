71372b7f141ce22669e2175b76a8b4f83e01a056
Elena <z-len@yandex.ru>
2025-07-19 14:22:02 +0300

    nothing

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,7 +1,15 @@
 console.log('script work!');
+
+
 let burger = document.querySelector('.js-burger');
+let menu = document.querySelector('.js-menu');
+
 burger.addEventListener('click', function(event) {
     event.preventDefault();
-    let menu = document.querySelector('.js-menu');
-    menu.classList.add('open');
+   
+    if( menu.style.display == 'block'){
+         menu.style.display = '';
+         return;
+    }
+    menu.style.display = 'block';
 })
